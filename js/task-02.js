19c8112a3e4260bd5b0dd06dd8bdca7ac5bcc6e3
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-08-21 09:23:25 +0300

    innicialithation of repository

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const newIngredients = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+document.querySelector("#ingredients").append(...newIngredients);
+console.log(newIngredients);
