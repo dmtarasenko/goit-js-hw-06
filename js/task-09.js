91ed36dc710c2384d65e64f2222a38b6cca3b196
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-08-22 00:16:57 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const currentColorEl = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onClickChangeColorBtn);
+
+function onClickChangeColorBtn() {
+  const color = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = color;
+  currentColorEl.textContent = color;
+}
