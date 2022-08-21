818ff7b53b817653c06ff58dda55cd79cce530ec
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-08-21 23:52:45 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  email.value === "" || password.value === ""
+    ? alert("Всі поля повинні бути заповнені")
+    : console.log({ email: `${email.value}`, password: `${password.value}` });
+
+  event.currentTarget.reset();
+}
